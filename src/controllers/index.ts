d651019dae3b28c006dc9c96c5cdff78caa0a91e
milkyKim <kimminji122258@gmail.com>
2021-07-06 01:04:22 +0900

    [feat] 설정 기능 구조 세팅

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,11 +2,13 @@ import userController from "./user";
 import reminderController from "./reminder";
 import randomController from "./random";
 import friendController from "./friend";
+import settingController from "./setting";
 
 export{
     userController,
     reminderController,
     randomController,
-    friendController
+    friendController,
+    settingController
 }
 
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,8 +2,9 @@ import express from "express";
 import userRouter from "./user";
 import reminderRouter from "./reminder";
 import randomRouter from "./random";
-import myRouter from "./my"
-import friendRouter from "./frined"
+import myRouter from "./my";
+import friendRouter from "./frined";
+import settingRouter from "./setting";
 
 const router = express();
 
@@ -12,5 +13,6 @@ router.use('/reminder', reminderRouter);
 router.use('/my', myRouter);
 router.use('/random',randomRouter);
 router.use('/friend',friendRouter);
+router.use('/setting',settingRouter);
 
 export default router;
