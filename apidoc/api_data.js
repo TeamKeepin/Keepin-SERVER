df19d1009e2fb93eecd7939dc7fec01439f0ddbd
expresshihway <03wow22@gmail.com>
2021-07-06 20:07:08 +0900

    Friend, Keepin API 작성

--- apidoc/api_data.js
+++ apidoc/api_data.js
@@ -1,6 +1,282 @@
 define({ "api": [
   {
     "type": "post",
+    "url": "/friend",
+    "title": "친구 생성",
+    "version": "1.0.0",
+    "name": "createFriend",
+    "group": "Friend",
+    "header": {
+      "examples": [
+        {
+          "title": "Header-Example:",
+          "content": "{\n    \"Content-Type\": \"application/json\"\n    \"jwt\": \"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwZ~~\"\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "parameter": {
+      "examples": [
+        {
+          "title": "Request-Example:",
+          "content": "{\n    \"name\": \"보리\"\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "success": {
+      "examples": [
+        {
+          "title": "Success-Response:",
+          "content": "- 200 OK\n{\n    \"status\": 201,\n    \"message\": \"친구 등록 성공\",\n    \"name\": \"보리\"\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "error": {
+      "examples": [
+        {
+          "title": "Error-Response:",
+          "content": "- 400 요청바디가 없음\n{\n    \"status\": 400,\n    \"message\": \"필수 정보(name))를 입력하세요.\"\n}\n\n- 400 중복된 값\n{\n    \"status\": 400,\n    \"message\": \"중복된 친구가 있습니다.\"\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "filename": "src/controllers/friend.ts",
+    "groupTitle": "Friend"
+  },
+  {
+    "type": "get",
+    "url": "/friend/search?name=keyword",
+    "title": "친구 검색 조회",
+    "version": "1.0.0",
+    "name": "searchFriends",
+    "group": "Friend",
+    "header": {
+      "examples": [
+        {
+          "title": "Header-Example:",
+          "content": "{\n    \"Content-Type\": \"application/json\"\n    \"jwt\": \"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwZ~~\"\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "parameter": {
+      "examples": [
+        {
+          "title": "Request-Example:",
+          "content": "\n- keyword에 검색할 단어를 넣음\n{\n    \"name\": \"보\" \n}",
+          "type": "json"
+        }
+      ]
+    },
+    "success": {
+      "examples": [
+        {
+          "title": "Success-Response:",
+          "content": "- 200 OK\n{\n    \"status\": 200,\n    \"message\": \"친구 검색 성공\",\n    \"data\": {\n        \"friends\": [\n            {\n                \"keepinIdx\": [],\n                \"_id\": \"60e416d15d759051988d18d0\",\n                \"name\": \"보리\"\n            }\n        ]\n    }\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "error": {
+      "examples": [
+        {
+          "title": "Error-Response:",
+          "content": "- 400 등록된 친구가 없을 경우\n{\n    \"status\": 400,\n    \"message\": \"등록된 친구들이 없습니다.\"\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "filename": "src/controllers/friend.ts",
+    "groupTitle": "Friend"
+  },
+  {
+    "type": "post",
+    "url": "/keepin",
+    "title": "키핀하기 생성",
+    "version": "1.0.0",
+    "name": "createKeepin",
+    "group": "Keepin",
+    "header": {
+      "examples": [
+        {
+          "title": "Header-Example:",
+          "content": "{\n    \"Content-Type\": \"application/json\"\n    \"jwt\": \"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwZ~~\"\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "parameter": {
+      "examples": [
+        {
+          "title": "Request-Example:",
+          "content": "* taken: 준/받은 여부 -> taken: true이면 받은\n* friendIdx: friend name을 표시하기 위함\n\n{\n    \"title\": \"보리 생일\",\n    \"photo\": \"보리가 좋아하는 강아지 김밥\",\n    \"taken\": false,\n    \"date\": \"20211202\",\n    \"category\": [\"생일\", \"축하\"],\n    \"record\": \"우리 보리의 첫돌. 이대로만 쑥쑥 커다오. 우리가족과 함께 해줘서 고마워.\",\n    \"friendIdx\":[\"60e416d15d759051988d18d0\", \"60e416d95d759051988d18d3\"]\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "success": {
+      "examples": [
+        {
+          "title": "Success-Response:",
+          "content": "- 200 OK\n{\n    \"status\": 200,\n    \"message\": \"키핀하기 생성 성공\",\n    \"keepin\": {\n        \"_id\": \"60e1d4070e50e39654b4bb5f\",\n        \"title\": \"보리 생일\",\n        \"photo\": \"보리가 좋아하는 강아지 김밥\",\n        \"taken\": false,\n        \"date\": \"2021.12.02\",\n        \"category\": [\n            \"생일\",\n            \"축하\"\n        ],\n        \"record\": \"우리 보리의 첫돌. 이대로만 쑥쑥 커다오. 우리가족과 함께 해줘서 고마워.\",\n        \"friendIdx\": [\n            \"60e416d15d759051988d18d0\",\n            \"60e416d95d759051988d18d3\"\n        ]\n    }\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "error": {
+      "examples": [
+        {
+          "title": "Error-Response:",
+          "content": "- 400 요청바디가 없음\n{\n    \"status\": 400,\n    \"message\": \"필수 정보를 입력하세요.\"\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "filename": "src/controllers/keepin.ts",
+    "groupTitle": "Keepin"
+  },
+  {
+    "type": "get",
+    "url": "/keepin/detail/:keepinIdx",
+    "title": "모아보기 상세페이지 조회",
+    "version": "1.0.0",
+    "name": "getDetailKeepin",
+    "group": "Keepin",
+    "header": {
+      "examples": [
+        {
+          "title": "Header-Example:",
+          "content": "{\n    \"Content-Type\": \"application/json\"\n    \"jwt\": \"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwZ~~\"\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "parameter": {
+      "examples": [
+        {
+          "title": "Request-Example:",
+          "content": "* [params] keepinIdx: 키핀 아이디에 해당하는 게시물",
+          "type": "json"
+        }
+      ]
+    },
+    "success": {
+      "examples": [
+        {
+          "title": "Success-Response:",
+          "content": "- 200 OK\n{\n    \"status\": 200,\n    \"message\": \"키핀 상세페이지 조회 성공\",\n    \"keepin\": {\n        \"userIdx\": \"60e1d4070e50e39654b4bb5f\",\n        \"keepinIdx\": \"60e42158909d3063102be165\",\n        \"title\": \"보리 생일\",\n        \"photo\": \"보리가 좋아하는 강아지 김밥\",\n        \"friends\": [\n            \"보리\",\n            \"밀키\"\n        ],\n        \"record\": \"우리 보리의 첫돌. 이대로만 쑥쑥 커다오. 우리가족과 함께 해줘서 고마워.\",\n        \"cateogry\": [\n            \"생일\",\n            \"축하\"\n        ],\n        \"date\": \"2021.12.02\",\n        \"taken\": false\n    }\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "error": {
+      "examples": [
+        {
+          "title": "Error-Response:",
+          "content": "- 400 요청바디가 빈 경우\n{\n    \"status\": 400,\n    \"message\": \"요청바디가 없습니다\".\"\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "filename": "src/controllers/keepin.ts",
+    "groupTitle": "Keepin"
+  },
+  {
+    "type": "get",
+    "url": "/keepin?taken=true",
+    "title": "모아보기 준/받은 조회",
+    "version": "1.0.0",
+    "name": "getTakenKeepin",
+    "group": "Keepin",
+    "header": {
+      "examples": [
+        {
+          "title": "Header-Example:",
+          "content": "{\n    \"Content-Type\": \"application/json\"\n    \"jwt\": \"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwZ~~\"\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "parameter": {
+      "examples": [
+        {
+          "title": "Request-Example:",
+          "content": "* [Querystring] taken: 준/받은 여부 -> taken: true이면 받은",
+          "type": "json"
+        }
+      ]
+    },
+    "success": {
+      "examples": [
+        {
+          "title": "Success-Response:",
+          "content": "- 200 OK\n{\n    \"status\": 200,\n    \"message\": \"모아보기 준/받은 조회 성공\",\n    \"keepin\": [\n        {\n            \"taken\": true,\n            \"_id\": \"60e420f9909d3063102be161\",\n            \"title\": \"PM이 탕수육 사줬지롱\",\n            \"photo\": \"탕수육 사진\",\n            \"date\": \"2021.06.21\"\n        }\n    ]\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "error": {
+      "examples": [
+        {
+          "title": "Error-Response:",
+          "content": "- 400 taken이 빈 값인 경우\n{\n    \"status\": 400,\n    \"message\": \"준/받은 여부를 선택하세요.\"\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "filename": "src/controllers/keepin.ts",
+    "groupTitle": "Keepin"
+  },
+  {
+    "type": "get",
+    "url": "/keepin/all?title=keyword",
+    "title": "모아보기 검색어 조회",
+    "version": "1.0.0",
+    "name": "searchKeepin",
+    "group": "Keepin",
+    "header": {
+      "examples": [
+        {
+          "title": "Header-Example:",
+          "content": "{\n    \"Content-Type\": \"application/json\"\n    \"jwt\": \"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwZ~~\"\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "parameter": {
+      "examples": [
+        {
+          "title": "Request-Example:",
+          "content": "* [Querystring] title: 제목으로 검색",
+          "type": "json"
+        }
+      ]
+    },
+    "success": {
+      "examples": [
+        {
+          "title": "Success-Response:",
+          "content": "- 200 OK\n{\n    \"status\": 200,\n    \"message\": \"키핀 검색어 조회 성공\",\n    \"data\": [\n        {\n            \"taken\": true,\n            \"_id\": \"60e420f9909d3063102be161\",\n            \"title\": \"PM이 탕수육 사줬지롱\",\n            \"photo\": \"탕수육 사진\",\n            \"date\": \"2021.06.21\"\n        }\n    ]\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "error": {
+      "examples": [
+        {
+          "title": "Error-Response:",
+          "content": "- 400 요청바디가 빈 경우\n{\n    \"status\": 400,\n    \"message\": \"요청바디가 없습니다\".\"\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "filename": "src/controllers/keepin.ts",
+    "groupTitle": "Keepin"
+  },
+  {
+    "type": "post",
     "url": "/reminder",
     "title": "리마인더 생성",
     "version": "1.0.0",
