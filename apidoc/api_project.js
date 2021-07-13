fe24c0e145fcba83a63fce2bbb96f2d7f6862537
milkyKim <kimminji122258@gmail.com>
2021-07-14 02:36:01 +0900

    [feat] 리마인더 수정 api 추가

--- apidoc/api_project.js
+++ apidoc/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-07-13T09:25:58.730Z",
+    "time": "2021-07-13T17:35:44.347Z",
     "url": "https://apidocjs.com",
     "version": "0.28.1"
   }
