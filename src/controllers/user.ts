70855d85d72dedf71118abd535c6acd16d58a4ba
유영우 <fbduddn97@naver.com>
2021-09-11 13:36:49 +0900

    [fix] 비밀번호 찾기 smtp port 변경

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -643,8 +643,8 @@ const findPassword = async (req, res) => {
 
     const transporter = nodemailer.createTransport({
       service: 'gmail',
-      port: 587,
-      secure: false, // true for 465, false for other ports
+      port: 465,
+      secure: true, // true for 465, false for other ports
       auth: { // 이메일을 보낼 계정 데이터 입력
         user:  'officialkeepin@gmail.com',
         pass:  config.keepinPassword,
